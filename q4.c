c78361b7a572cd38ccc9052298652004065c4072
parth0306 <25121cn146@glbitm.ac.in>
2026-02-25 21:09:58 +0530

    q4.c
    write a program that accept centigrade temperature and convert it into fahrenheit.

---
+++ q4.c
@@ -0,0 +1,11 @@
+//write a program that accept centigrate temperature and convert it into fahrenheit.
+#include <stdio.h>
+int main()
+{
+    float c,f;
+    printf("enter temperature in cantigrade :");
+    scanf("\n%f",&c),
+    f=(9*c/5)+32 ;
+    printf("required temperature in fahrenheit : %f",f);
+    return 0;
+}
