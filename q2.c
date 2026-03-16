13493c1cfee0ce59362e88acc230c784b6af4f4e
parth0306 <25121cn146@glbitm.ac.in>
2026-03-16 20:05:01 +0530

    Change printf format for interest outputs
    Updated the output format for interest calculations to use default float representation instead of fixed decimal places.

--- q2.c
+++ q2.c
@@ -1,6 +1,5 @@
-//Write a program that calculate the sample interest and compound interest . The principal amount, rate of interest and time are entered through the keyboard.
+//Write a program that calculate the sample interest and compound interest.The principal amount, rate of interest and time are entered through the keyboard.
 #include <stdio.h>
-#include <math.h>
 int main() {
 float principal, rate, time, simple_interest, compound_interest, amount;
     printf("\nEnter principal amount: ");
@@ -12,8 +11,8 @@ float principal, rate, time, simple_interest, compound_interest, amount;
     simple_interest = (principal * rate * time) / 100;
     amount = principal * pow((1 + rate / 100), time);
     compound_interest = amount - principal;
-    printf("\nSimple Interest: %.2f\n", simple_interest);
-    printf("Compound Interest: %.2f\n", compound_interest);
+    printf("\nSimple Interest: %f\n", simple_interest);
+    printf("Compound Interest: %f\n", compound_interest);
 
     return 0;
 }
