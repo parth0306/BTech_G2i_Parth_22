fba56f277e93a6c07653af92e226e30f9b5b2184
parth0306 <25121cn146@glbitm.ac.in>
2026-03-19 10:38:38 +0530

    Reverse digits using loop

---
+++ Reverse digits using loop.c
@@ -0,0 +1,16 @@
+//Write a program to reverse the digits of a no. using loop.
+#include <stdio.h>
+int main() 
+{
+int n,r,m=0;
+printf("Enter the number : ");
+scanf("%d", &n); 
+while (n != 0) 
+{
+    r=n%10;
+    m=m*10+r;
+    n=n/10;
+}
+printf("reversed number is %d \t",m);
+ return 0;
+}
