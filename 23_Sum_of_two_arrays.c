772ad6caf202345242c304ad9836c31400a28f69
parth0306 <25121cn146@glbitm.ac.in>
2026-04-15 20:05:50 +0530

    sum of two arrays and save the sum in third array

---
+++ 23_Sum_of_two_arrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i ,arr1[n], arr2[n], sumArr[n];
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements of first array:\n");
+    for(i = 0; i < n; i++) 
+    {scanf("%d", &arr1[i]);}
+    printf("Enter elements of second array:\n");
+    for(i = 0; i < n; i++) 
+    {scanf("%d", &arr2[i]);}
+    for(i = 0; i < n; i++) 
+    {sumArr[i] = arr1[i] + arr2[i];}
+    printf("Sum of corresponding elements (third array):\n");
+    for(i = 0; i < n; i++) 
+    {printf("%d ", sumArr[i]);}
+    return 0;
+}
