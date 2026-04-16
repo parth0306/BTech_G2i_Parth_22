e8f31072ab7724ed15af296d621929dc3ffadfb2
parth0306 <25121cn146@glbitm.ac.in>
2026-04-16 11:48:32 +0530

    multiply and add 2 matrices if possible

--- 30_Matrices_multiplication_and_addition.c
+++ 30_Matrices_multiplication_and_addition.c
@@ -17,29 +17,30 @@ int main() {
 
     // Input elements 
     printf("\nEnter elements of matrix 1:\n");
-    for (i = 0; i < r1; ++i)
-        for (j = 0; j < c1; ++j) 
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c1; j++) 
             scanf("%d", &a[i][j]);
 
     printf("Enter elements of matrix 2:\n");
-    for (i = 0; i < r2; ++i)
-        for (j = 0; j < c2; ++j) 
+    for (i = 0; i < r2; i++)
+        for (j = 0; j < c2; j++) 
             scanf("%d", &b[i][j]);
 
     // Matrix Multiplication
-    for (i = 0; i < r1; ++i) {
-        for (j = 0; j < c2; ++j) 
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) 
         {
             prod[i][j] = 0;
-            for (k = 0; k < c1; ++k) 
+            for (k = 0; k < c1; k++) 
                 prod[i][j] += a[i][k] * b[k][j];
         }
     }
 
     // Output Results
     printf("\nProduct Matrix:\n");
-    for (i = 0; i < r1; ++i) {
-        for (j = 0; j < c2; ++j) printf("%d  ", prod[i][j]);
+    for (i = 0; i < r1;i++) {
+        for (j = 0; j < c2; j++) 
+        printf("%d  ", prod[i][j]);
         printf("\n");
     }
 
@@ -48,13 +49,15 @@ int main() {
     
     if (r1==r2 && c2==c2) 
     {
-        for (i = 0; i < r1; ++i)
-            for (j = 0; j < c1; ++j) sum[i][j] = a[i][j] + b[i][j];
+        for (i = 0; i < r1; i++)
+            for (j = 0; j < c1; j++)
+            sum[i][j] = a[i][j] + b[i][j];
     
         printf("\nSum Matrix:\n");
-        for (i = 0; i < r1; ++i) 
+        for (i = 0; i < r1; i++) 
         {
-            for (j = 0; j < c1; ++j) printf("%d  ", sum[i][j]);
+            for (j = 0; j < c1; j++)
+            printf("%d  ", sum[i][j]);
             printf("\n");
         }
     } else printf("\nAddition not possible: Different dimensions.\n");
