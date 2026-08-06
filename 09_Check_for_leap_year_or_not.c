455d36350cf2b8299cf97edfa4a9cd23d404b27d
parth0306 <25121cn146@glbitm.ac.in>
2026-08-07 00:17:36 +0530

    Fix leap year logic and improve output formatting

--- 09_Check_for_leap_year_or_not.c
+++ 09_Check_for_leap_year_or_not.c
@@ -1,13 +1,19 @@
-// Program to tell whether a year is a leap year or not.
 #include <stdio.h>
 
-int main() 
+int main()
 {
     int a;
-    printf("enter year:");
-    scanf("%d",&a);
-    if((a%4==a && a%100 !=0)||(a%400==0))
-    {printf("\n This is a leap year");}
-    else printf("\n This is not a leap year");
+    printf("Enter year: ");
+    scanf("%d", &a);
+
+    if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))
+    {
+        printf("This is a leap year\n");
+    }
+    else
+    {
+        printf("This is not a leap year\n");
+    }
+
     return 0;
 }
